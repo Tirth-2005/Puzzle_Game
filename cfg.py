e70d361d1e23d991e9d407acdc557b27c90cac00
Tirth-2005 <tirthpanchal2825@gmail.com>
2024-04-14 13:48:49 +0530

    Puzzle Game

---
+++ cfg.py
@@ -0,0 +1,15 @@
+import os
+
+S_SIZE = (640, 640)
+PIC_ROOT_DIR = os.path.join(os.getcwd(), 'Puzzle/resources/pictures')
+FONTPATH = os.path.join(os.getcwd(), 'Puzzle/resources/font/FZSTK.TTF')
+#print(PIC_ROOT_DIR)
+#print(FONTPATH)
+BGCOLOR = (255, 255, 255)
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+FPS = 40
+RANDNUM = 100
